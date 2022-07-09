e806a34cb97644989a9afeec0ba119ca9b060d8a
andantet <orrdaniel73@gmail.com>
2022-07-09 19:32:32 +0100

    pause animations on pause

---
+++ src/main/java/me/andante/test/mixin/client/AnimationsStopOnPause.java
@@ -0,0 +1,21 @@
+package me.andante.test.mixin.client;
+
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.texture.Sprite;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+public class AnimationsStopOnPause {
+    @Environment(EnvType.CLIENT)
+    @Mixin(Sprite.Animation.class)
+    public static class SpriteAnimationMixin {
+        @Inject(method = "tick", at = @At("HEAD"), cancellable = true)
+        private void onTick(CallbackInfo ci) {
+            if (MinecraftClient.getInstance().isPaused()) ci.cancel();
+        }
+    }
+}
