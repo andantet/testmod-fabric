1b1f402116bebc61d613367c983c2488deb91227
andantet <orrdaniel73@gmail.com>
2022-09-14 19:51:09 +0100

    cut it off, at its toes.

---
+++ src/main/java/me/andante/nochickenrender/mixin/client/EntityRenderDispatcherMixin.java
@@ -0,0 +1,23 @@
+package me.andante.nochickenrender.mixin.client;
+
+import me.andante.nochickenrender.NoChickenRender;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.minecraft.client.render.Frustum;
+import net.minecraft.client.render.entity.EntityRenderDispatcher;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.passive.ChickenEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Environment(EnvType.CLIENT)
+@Mixin(EntityRenderDispatcher.class)
+public class EntityRenderDispatcherMixin {
+    @Inject(method = "shouldRender", at = @At("HEAD"), cancellable = true)
+    private <E extends Entity> void onShouldRender(E entity, Frustum frustum, double x, double y, double z, CallbackInfoReturnable<Boolean> cir) {
+        if (!NoChickenRender.ENABLED) return;
+        if (entity instanceof ChickenEntity) cir.setReturnValue(false);
+    }
+}
